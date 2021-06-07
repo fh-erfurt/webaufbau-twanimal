f87e2a2743e96a490f3c3ffed6398fbd87a804d6
Luca Voges <vogeslu@gmail.com>
2021-06-07 08:12:12 +0200

    fixed missing sass dependencies added react router dom added router provider and some routes added empty route for profile and registration renamed css-files to scss-files modified plain scss/css files to module-files moved scss/css-files into assets-folder moved routes outside of components

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import "../assets/css/components/postContent.css";
+import React, { Component } from "react"
+
+import styles from "../assets/css/components/postContent.module.scss"
 
 class PostContent extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="postContent">
-        <p>post Content</p>
-      </div>
-    );
-  }
+    state = {}
+    render() {
+        return (
+            <div className={styles.postContent}>
+                <p>post Content</p>
+            </div>
+        )
+    }
 }
 
-export default PostContent;
-<div></div>;
+export default PostContent
---
+++ src/routes/Login.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch, faPaw } from "@fortawesome/free-solid-svg-icons"
+import style from "../assets/css/routes/login.module.scss"
+
+class Login extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className={style.formTextContainer}>
+                    <div className={style.contentWraperImage}>
+                        <div className={style.logo}>
+                            <FontAwesomeIcon
+                                className={style.icons}
+                                icon={faPaw}
+                            />
+                        </div>
+                    </div>
+                    <div className={style.text}>
+                        <p>Melde dich bei Twanimal an</p>
+                    </div>
+                    <div className={style.form}>
+                        <form action="#">
+                            <div className={style.inputs}>
+                                <input type="email" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                            </div>
+                            <div className={style.submitButton}>
+                                <button>Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className={style.link}>
+                        <a href="#">
+                            Noch nicht Teil der Crew? Hier geht's zur
+                            Registration
+                        </a>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Login
