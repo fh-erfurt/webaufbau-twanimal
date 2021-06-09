4e85fc79c93c22e32450dde7c0332cb1efbd0f2a
Salma-Alkhaiyal <73823841+Salma-Alkhaiyal@users.noreply.github.com>
2021-06-09 14:02:40 +0200

    Update for Profile und postContent

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,14 +1,58 @@
 import React, { Component } from "react"
-
-import styles from "../assets/css/components/postContent.module.scss"
+import style from "../assets/css/components/postContent.module.scss"
 
 class PostContent extends Component {
     state = {}
     render() {
         return (
-            <div className={styles.postContent}>
-                <p>post Content</p>
-            </div>
+            <React.Fragment>
+                <div className={style.postContent}>
+                    <div className={style.profileImage}>
+                        <img
+                            src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+                            alt="profileImage"
+                        />
+                    </div>
+
+                    <div className={style.status}>
+                        <p>Was gibt es neues?</p>
+                    </div>
+
+                    <div className={style.newPost}>
+                        <button>Wuff</button>
+                    </div>
+                </div>
+                <div className={style.post}>
+                    <div className={style.pImage}>
+                        <img
+                            src="https://cdn.discordapp.com/attachments/715508471293935669/851834287971631134/greg-rosenke-GGGOnJKb3KM-unsplash_1.jpg"
+                            alt="pImage"
+                        />
+                    </div>
+
+                    <div className={style.profileName}>
+                        <b>Schneckbert</b>
+                        <span>@Schneki_schneck</span>
+                    </div>
+                    <div className={style.info}>
+                            <b>2 Uhr</b> 
+                    </div>
+                    <div className={style.Neu}>
+                        <p>
+                        Hallo Leute!
+                        Hier ist Schneckbert, ich war heute mal wieder ganz fix unterwegs. Außerdem hatte ich heute mein neues Outfit an. 
+                        Wie findet ihr das? #schnecksi
+                        </p>
+                        <div className={style.Image}>
+                            <img
+                                src="https://cdn.discordapp.com/attachments/715508471293935669/851834468070588436/zdenek-machacek-_9bRrDyOQTQ-unsplash.jpg"
+                                alt="Schneckimage"
+                            />
+                        </div>
+                    </div>
+                   
+                </div>
+            </React.Fragment>
         )
     }
 }
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,66 @@
 import React, { Component } from "react"
+import Navigation from "../components/Navigation"
+import style from "../assets/css/routes/profile.module.scss" 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch, faPaw } from "@fortawesome/free-solid-svg-icons"
+import Suggestions from "../components/Suggestions"
+
 class Profile extends Component {
     render() {
-        return <React.Fragment>Profile</React.Fragment>
+        return <React.Fragment>
+            <Navigation />
+           
+            <div className={style.background}>
+            <Suggestions/>
+                
+            </div>
+            <div className={style.content}>
+                
+                <div className={style.profile}>
+                    <div className={style.profileImage}>
+                        <img
+                            src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+                            alt="profileImage"
+                        />
+                    </div>
+                    
+                </div>
+                
+                <div className={style.profileName}>
+                        <b>Hannelore</b>
+                        <span>@hanni_the_dog</span>
+                </div>
+
+                <div className={style.info}>
+                    <div className={style.posts}>
+                        <b>12 Beiträge</b>
+                    </div>
+                    <div className={style.posts}>
+                        <b>400 Follower</b>
+                    </div>
+                    <div className={style.posts}>
+                        <b>40 Freunde</b>
+                    </div>
+                </div>
+
+                <div className={style.status}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        ut aliquam, purus sit amet luctus venenatis, lectus
+                        magna fringilla urna, porttitor rhoncus dolor purus non
+                        enim praesent elementum facilisis leo, vel
+                    </p>
+
+                </div>
+
+                <div className={style.editProfil}>  
+                        <button>Profil bearbeiten</button>
+                </div>
+            </div>
+
+               
+                
+        </React.Fragment>
     }
 }
 
