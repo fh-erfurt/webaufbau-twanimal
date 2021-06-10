5627877728a853e022765efa69c18f65349d9c08
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-06-10 22:39:39 +0200

    update properties-homePge & navbar

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,75 +1,64 @@
-import React, { Component } from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSearch, faPaw } from "@fortawesome/free-solid-svg-icons"
-import style from "../assets/css/components/profileCard.module.scss"
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faPaw } from "@fortawesome/free-solid-svg-icons";
+import style from "../assets/css/components/profileCard.module.scss";
 
 class ProfileCard extends Component {
-    constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			user: this.props.user
-		}
-	}
+    this.state = {
+      user: this.props.user,
+    };
+  }
 
-    state = {}
-    render() {
-        return (
-            <React.Fragment>
-                <div className={style.searchField}>
-                    <form action="#">
-                        <FontAwesomeIcon
-                            className={style.icons}
-                            icon={faSearch}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Twanimal durchsuchen..."
-                            name="search"
-                        />
-                    </form>
-                </div>
-                <div className={style.profile}>
-                    <div className={style.profileImage}>
-                        <img
-                            src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-                            alt="profileImage"
-                        />
-                    </div>
-                    <div className={style.profileName}>
-                        <b>{ this.state.user.displayName }</b>
-                        <span>{ this.state.user.username }</span>
-                    </div>
-                </div>
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className={style.searchField}>
+          <form action="#">
+            <FontAwesomeIcon className={style.icons} icon={faSearch} />
+            <input
+              type="text"
+              placeholder="Twanimal durchsuchen..."
+              name="search"
+            />
+          </form>
+        </div>
+        <div className={style.profile}>
+          <div className={style.profileImage}>
+            <img src={this.state.user.photo} alt="profileImage" />
+          </div>
+          <div className={style.profileName}>
+            <b>{this.state.user.displayName}</b>
+            <span>{this.state.user.username}</span>
+          </div>
+        </div>
 
-                <div className={style.info}>
-                    <div className={style.posts}>
-                        <b>12 Beiträge</b>
-                    </div>
-                    <div className={style.posts}>
-                        <b>400 Follower</b>
-                    </div>
-                    <div className={style.posts}>
-                        <b>40 Freunde</b>
-                    </div>
-                </div>
+        <div className={style.info}>
+          <div className={style.posts}>
+            <b>{this.state.user.numberOfAllPosts} Beiträge</b>
+          </div>
+          <div className={style.posts}>
+            <b>{this.state.user.numberOfAllFollower} Follower</b>
+          </div>
+          <div className={style.posts}>
+            <b>{this.state.user.namberOfAllFriends} Freunde</b>
+          </div>
+        </div>
 
-                <div className={style.status}>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ut aliquam, purus sit amet luctus venenatis, lectus
-                        magna fringilla urna, porttitor rhoncus dolor purus non
-                        enim praesent elementum facilisis leo, vel
-                    </p>
-                </div>
+        <div className={style.status}>
+          <p>{this.state.user.status}</p>
+        </div>
 
-                <div className={style.newPost}>
-                    <FontAwesomeIcon className={style.icons} icon={faPaw} />
-                    <button>Neuer Beitrag</button>
-                </div>
-            </React.Fragment>
-        )
-    }
+        <div className={style.newPost}>
+          <FontAwesomeIcon className={style.icons} icon={faPaw} />
+          <button>Neuer Beitrag</button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default ProfileCard
+export default ProfileCard;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,45 @@
-import React, { Component } from "react"
-import Navigation from "../components/Navigation"
-import ProfileCard from "../components/ProfileCard"
-import PostContent from "../components/PostContent"
+import React, { Component } from "react";
+import Navigation from "../components/Navigation";
+import ProfileCard from "../components/ProfileCard";
+import PostContent from "../components/PostContent";
 
-import style from "../assets/css/routes/home.module.scss"
+import style from "../assets/css/routes/home.module.scss";
 
 class Home extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <header className="App-header">
-                    <Navigation />
-                </header>
-                <div className={style.content}>
-                    <div className={style.postContent}>
-                        <PostContent />
-                    </div>
-                    <div className={style.profileCard}>
-                        <ProfileCard user={{ username: 'Hanni', displayName: 'the dog' }} />
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <header className="App-header">
+          <Navigation
+            user={{
+              username: "Hannelore",
+              displayName: "@hanni_the_dog",
+            }}
+          />
+        </header>
+        <div className={style.content}>
+          <div className={style.postContent}>
+            <PostContent />
+          </div>
+          <div className={style.profileCard}>
+            <ProfileCard
+              user={{
+                photo:
+                  "https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80",
+                username: "Hanni",
+                displayName: "the dog",
+                numberOfAllPosts: 12,
+                numberOfAllFollower: 400,
+                namberOfAllFriends: 40,
+                status:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel",
+              }}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Home
+export default Home;
