c06946dd907b04649a6757da444d841b15ea4d6f
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-06-16 15:11:35 +0200

    update photo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -46,8 +46,7 @@ class Navigation extends Component {
               <div
                 className={styles.profileImage}
                 style={{
-                  backgroundImage:
-                    "url(https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80)",
+                  backgroundImage: `url(${this.state.user.photo})`,
                 }}
               ></div>
             </Link>
