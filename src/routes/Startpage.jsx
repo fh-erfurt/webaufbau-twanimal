ae6af182211677a4f450ac31c22d4e3cb312054c
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-06-30 15:10:35 +0200

    update path

--- src/routes/Startpage.jsx
+++ src/routes/Startpage.jsx
@@ -7,60 +7,55 @@ import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 
 class Startpage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className={style.verticalCenter}>
-                    <div className={style.formTextContainer}>
-                        <div className={style.contentWraperImage}>
-                            <div className={style.logo}>
-                                <FontAwesomeIcon
-                                    className={style.icons}
-                                    icon={faPaw}
-                                />
-                            </div>
-                        </div>
-                        <div className={style.heading}>
-                            <h1>Gib deinem Haustier eine Stimme!</h1>
-                        </div>
-                        <div className={style.text}>
-                            <p>Willkommen bei Twanimal</p>
-                        </div>
-                        <div className={style.startpageButtons}>
-                            <div className={style.loginButton}>
-                                <Link to="/login">
-                                    <button>
-                                        <FontAwesomeIcon 
-                                            className={style.buttonIcon} 
-                                            icon={faPaw} 
-                                        />
-                                        Anmelden
-                                    </button>
-                                </Link>
-                            </div>
-                            <div className={style.registrationButton}>
-                                <Link to="/registration">
-                                    <button>
-                                        <FontAwesomeIcon 
-                                            className={style.buttonIcon} 
-                                            icon={faPaw} 
-                                        />
-                                        Registrieren
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className={style.link}>
-                            <Link to="/aboutus">
-                                Was ist Twanimal?
-                            </Link>
-                        </div>
-                    </div>
-                    <Footer />
-                </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className={style.verticalCenter}>
+          <div className={style.formTextContainer}>
+            <div className={style.contentWraperImage}>
+              <div className={style.logo}>
+                <FontAwesomeIcon className={style.icons} icon={faPaw} />
+              </div>
+            </div>
+            <div className={style.heading}>
+              <h1>Gib deinem Haustier eine Stimme!</h1>
+            </div>
+            <div className={style.text}>
+              <p>Willkommen bei Twanimal</p>
+            </div>
+            <div className={style.startpageButtons}>
+              <div className={style.loginButton}>
+                <Link to="/login">
+                  <button>
+                    <FontAwesomeIcon
+                      className={style.buttonIcon}
+                      icon={faPaw}
+                    />
+                    Anmelden
+                  </button>
+                </Link>
+              </div>
+              <div className={style.registrationButton}>
+                <Link to="/registration">
+                  <button>
+                    <FontAwesomeIcon
+                      className={style.buttonIcon}
+                      icon={faPaw}
+                    />
+                    Registrieren
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className={style.link}>
+              <Link to="/überuns">Was ist Twanimal?</Link>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Startpage
+export default Startpage;
