d0c8323c74eb3ef3fe8a01e74aa423d75af045ba
Luca Voges <vogeslu@gmail.com>
2021-07-06 13:28:00 +0200

    updated styles for postcontent added functionality to submit posts (with attachements)

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,61 +1,211 @@
+import {
+	faCircleNotch,
+	faImages,
+	faTimes,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React, { Component } from "react"
 import style from "../assets/css/components/postContent.module.scss"
+import config from "../config"
+import { authenticationService } from "../services/authenticationService"
 
 class PostContent extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          user: this.props.user,
-        };
-      }
-    state = {}
-    render() {
-        return (
-            <React.Fragment>
-                <div className={style.postContent}>
-                    <div className={style.profileImage}>
+	constructor(props) {
+		super(props)
 
-                        <img src={this.state.user.photo} alt="profileImage" />
+		this.state = {
+			user: authenticationService.getUser(),
+			attachements: [],
+			content: "",
+			isSubmitting: false,
+		}
 
-                    </div>
+		this.apiToken = authenticationService.getAPIToken()
+	}
 
-                    <div className={style.status}>
-                        <p>Was gibt es neues?</p>
-                    </div>
+	handleFile = (event) => {
+		if (this.state.attachements.length >= 4) return
 
-                    <div className={style.newPost}>
-                        <button>Wuff</button>
-                    </div>
-                </div>
-                <div className={style.post}>
-                    <div className={style.pImage}>
+		const files = event.target.files
+		const attachements = this.state.attachements
 
-                        <img src={this.state.user.picture} alt="pImage" />
-        
-                    </div>
+		for (let i = 0; i < files.length; i++) {
+			const file = files.item(i)
 
-                    <div className={style.profileName}>
-                        <b>{this.state.user.dName}</b>
-                        <span>{this.state.user.uName}</span>
-                    </div>
-                    <div className={style.info}>
-                        <b>{this.state.user.time}</b> 
-                    </div>
-                    <div className={style.Neu}>
-                        <p>{this.state.user.text}</p>
-                        <div className={style.Image}>
+			attachements.push({
+				file: file,
+				preview: URL.createObjectURL(file),
+			})
 
-                        <img src={this.state.user.sPicture} alt="Schneckimage" />
+			if (attachements.length >= 4) break
+		}
 
+		this.setState({
+			attachements: attachements,
+		})
+	}
 
-                        </div>
-                    </div>
-                   
-                </div>
-            </React.Fragment>
-        )
-    }
+	removeFile = (index) => {
+		const attachements = this.state.attachements
+
+		const file = attachements[index]
+
+		URL.revokeObjectURL(file.preview)
+
+		attachements.splice(index, 1)
+
+		this.setState({
+			attachements: attachements,
+		})
+	}
+
+	submitPost = async (event) => {
+		event.preventDefault()
+
+		if (this.state.isSubmitting || (this.state.attachements.length === 0 && this.state.content.length === 0)) return
+
+		this.setState({ isSubmitting: true })
+
+		const text = this.state.content
+		const attachements = this.state.attachements
+
+		const formData = new FormData()
+		formData.append("text", text)
+
+		for (const attachement of attachements)
+			formData.append("attachements", attachement.file)
+
+		const response = await fetch(`${config.apiHost}/post/new`, {
+			method: "post",
+			headers: {
+				Authorization: `Bearer ${this.apiToken}`,
+			},
+			body: formData,
+		})
+
+		if (response.ok) {
+			const post = await response.json()
+
+			for (const attachement of attachements)
+				URL.revokeObjectURL(attachement.preview)
+
+			this.setState({
+				isSubmitting: false,
+				attachements: [],
+				content: "",
+			})
+		} else {
+			const error = await response.json()
+			console.log(error)
+		}
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<form className={style.newPost} onSubmit={this.submitPost}>
+					<div className={style.newPostRow}>
+						<img
+							className={style.profileImage}
+							src={this.state.user.profilePicture}
+							alt="profileImage"
+						/>
+						<div className={style.postContent}>
+							<textarea
+								placeholder="Was gibts neues?"
+								value={this.state.content}
+								onChange={(e) =>
+									this.setState({ content: e.target.value })
+								}
+							></textarea>
+							{this.state.attachements.length > 0 && (
+								<div className={style.attachements}>
+									{this.state.attachements.map(
+										(attachement, index) => {
+											return (
+												<div
+													key={index}
+													className={
+														style.attachement
+													}
+													style={{
+														backgroundImage: `url(${attachement.preview})`,
+													}}
+												>
+													<div
+														onClick={() =>
+															this.removeFile(
+																index
+															)
+														}
+													>
+														<FontAwesomeIcon
+															icon={faTimes}
+														/>
+													</div>
+												</div>
+											)
+										}
+									)}
+								</div>
+							)}
+						</div>
+					</div>
+					<div className={style.actionRow}>
+						{this.state.attachements.length < 4 && (
+							<div className={style.imageButton}>
+								<FontAwesomeIcon
+									className={style.icons}
+									icon={faImages}
+								/>
+								<input
+									type="file"
+									accept="image/*"
+									multiple
+									onChange={this.handleFile}
+								/>
+							</div>
+						)}
+						<div className={style.spacer}></div>
+						<button className={ (this.state.attachements.length === 0 && this.state.content.length === 0) ? style.disabled : null }>
+							{this.state.isSubmitting && (
+								<span>
+									<FontAwesomeIcon
+										spin={true}
+										icon={faCircleNotch}
+									/>
+									&nbsp;
+								</span>
+							)}
+							Wuff
+						</button>
+					</div>
+				</form>
+				<div className={style.post}>
+					<div className={style.pImage}>
+						<img src={this.state.user.picture} alt="pImage" />
+					</div>
+
+					<div className={style.profileName}>
+						<b>{this.state.user.dName}</b>
+						<span>{this.state.user.uName}</span>
+					</div>
+					<div className={style.info}>
+						<b>{this.state.user.time}</b>
+					</div>
+					<div className={style.Neu}>
+						<p>{this.state.user.text}</p>
+						<div className={style.Image}>
+							<img
+								src={this.state.user.sPicture}
+								alt="Schneckimage"
+							/>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
 }
 
 export default PostContent
